5e35967bee6a3a8e326c76ff15a3d4557c900921
Andresito <alumno.577931@ies-azarquiel.es>
2023-06-08 10:54:04 +0200

    Indicamos que es la BETA

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -5,8 +5,16 @@ import Logo from './Logo';
 const Header = () => {
   return (
     <header className="header">
-      <Logo />
-      <h1>Domotiza2</h1>
+      <div className='menu'>
+        <h1 className="menu-text">=</h1>
+      </div>
+      <div className='centrado'>
+        <Logo />
+        <h1>Domotiza2</h1>
+      </div>
+      <div className='beta'>
+        <h1 className="beta-text">B E T A</h1>
+      </div>
     </header>
   );
 };
